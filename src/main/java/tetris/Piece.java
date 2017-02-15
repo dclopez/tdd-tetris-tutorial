98d06799a7008f1c8313785cbcf1f133a4605d7c
David <d.clopez@udc.es>
2017-02-15 18:21:15 +0100

    Pasando test can_be_rotated_right

--- src/main/java/tetris/Piece.java
+++ src/main/java/tetris/Piece.java
@@ -10,4 +10,38 @@ public class Piece {
 	public String toString(){
 		return this.composition;
 	}
+	
+	public Piece rotateRight(){
+		String pointingTop="" +
+                ".X.\n" +
+                ".X.\n" +
+                "...\n";
+		String pointingRight = "" +
+                "...\n" +
+                ".XX\n" +
+                "...\n";
+		String pointingDown = "" +
+                "...\n" +
+                ".X.\n" +
+                ".X.\n";
+		String pointingLeft= "" +
+                "...\n" +
+                "XX.\n" +
+                "...\n";
+		Piece piece = null;
+		
+		if (this.composition.equals(pointingTop)){
+			piece= new Piece(pointingRight);
+		}
+		if (this.composition.equals(pointingRight)){
+			piece= new Piece(pointingDown);
+		}
+		if (this.composition.equals(pointingDown)){
+			piece= new Piece(pointingLeft);
+		}
+		if(this.composition.equals(pointingLeft)){
+			piece= new Piece(pointingTop);
+			}
+		return piece;
+		}
 }
