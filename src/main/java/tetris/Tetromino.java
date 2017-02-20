009b532263fd262fa93f8524e7c1b29ab17b3d3d
David <d.clopez@udc.es>
2017-02-20 16:47:17 +0100

    Pasando test can_be_rotated_right_3_times

--- src/main/java/tetris/Tetromino.java
+++ src/main/java/tetris/Tetromino.java
@@ -16,8 +16,40 @@ public class Tetromino {
 		return this.shape;
 	}
 	
-	public void rotateRight(){
-		
+	public Tetromino rotateRight(){
+		String pointingTop="" +
+                ".T.\n" +
+                "TTT\n" +
+                "...\n";
+		String pointingRight = "" +
+                ".T.\n" +
+                ".TT\n" +
+                ".T.\n";
+		String pointingDown = "" +
+                "...\n" +
+                "TTT\n" +
+                ".T.\n";
+		String pointingLeft= "" +
+                ".T.\n" +
+                "TT.\n" +
+                ".T.\n";
+
+		Tetromino tetro = null;
+
+		if (this.shape.equals(pointingTop)){
+			tetro= new Tetromino(pointingRight);
+		}
+		if (this.shape.equals(pointingRight)){
+			tetro= new Tetromino(pointingDown);
+		}
+		if (this.shape.equals(pointingDown)){
+			tetro= new Tetromino(pointingLeft);
+		}
+		if(this.shape.equals(pointingLeft)){
+			tetro= new Tetromino(pointingTop);
+			}
+
+		return tetro;
 	}
 	
 	public void rotateLeft(){
